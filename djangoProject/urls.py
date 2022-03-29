30408350961e938b387ce810a3ba41ff1df6cbe5
reza <rezamahmoudian80@gmail.com>
2022-03-30 01:15:49 +0430

    add language to urls with i18n_patterns

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 
 
 urlpatterns = [
+]
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('SlideShow.urls')),
-]
+)
+
