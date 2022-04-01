3b676b702568dba5e48f5f82180ec0eca847ddcb
reza <rezamahmoudian80@gmail.com>
2022-04-01 11:19:49 +0430

    translation navbar vand boxes

--- SlideShow/models.py
+++ SlideShow/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.html import format_html
 from translated_fields import TranslatedField
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 
 # Create your models here.
@@ -12,18 +13,18 @@ class Article(models.Model):
             ('p', "منتشر شده"),
             ('d', "پیش نویس"),
     )
-    title = TranslatedField(models.CharField(max_length=200, verbose_name='عنوان', default=0))
-    slug = models.CharField(max_length=100, unique=True, verbose_name='آدرس')
-    content = models.TextField(verbose_name='محتوا')
-    image = models.ImageField(upload_to="images", verbose_name='تصویر')
-    publish = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
+    title = TranslatedField(models.CharField(_('title'), max_length=200, default=0))
+    slug = models.CharField(_('slug'), max_length=100, unique=True)
+    content = models.TextField(_('content'))
+    image = models.ImageField(_('image'), upload_to="images")
+    publish = models.DateTimeField(_('publish'), default=timezone.now)
     create = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, verbose_name='وضعیت')
+    status = models.CharField(_('status'), max_length=1, choices=STATUS_CHOICES)
 
     class Meta:
-        verbose_name = 'مقاله'
-        verbose_name_plural = 'مقالات'
+        verbose_name = _('article')
+        verbose_name_plural = _('articles')
         ordering = ['-publish']
 
     def __str__(self):
@@ -31,3 +32,5 @@ class Article(models.Model):
 
     def image_post(self):
         return format_html("<img width=150 height=160 border-radius: 5px; src='{}'>".format(self.image.url))
+
+    image_post.short_description = _('image')
